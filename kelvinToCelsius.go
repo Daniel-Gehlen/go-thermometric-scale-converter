12ecbfb3946557db7bd83841badd72b807f68eec
Daniel Gehlen <142283217+Daniel-Gehlen@users.noreply.github.com>
2024-04-11 21:19:07 -0300

    Update kelvinToCelsius.go

--- kelvinToCelsius.go
+++ kelvinToCelsius.go
@@ -1,1 +1,14 @@
+package main
 
+import "fmt"
+
+func main() {
+  // Valor do ponto de ebulição da água em Kelvin
+  kelvin := 373.15
+
+  // Convertendo para Celsius
+  celsius := kelvin - 273.15
+
+  // Apresentando o resultado
+  fmt.Printf("Ponto de ebulição da água em Celsius: %.2f °C\n", celsius)
+}
